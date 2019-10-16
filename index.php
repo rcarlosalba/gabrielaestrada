9f5acfc68dbc3d7d3dd6c2fe566c5a9b7ac77c0c
Carlos Alba <hola@carlosalba.net>
2019-10-16 16:50:45 +0300

    introduce, parallax, services

--- index.php
+++ index.php
@@ -4,9 +4,58 @@
         <?php include('header.php'); ?>
         <?php include('menu.php'); ?>
     </div>
-    <div class="space-top"></div>
-
     <?php include('carrousel.php')?>
+    <div class="space-top"></div>
+    <div class=" central-container introduce flex-between" id="introduce">
+        <div class="presentacion">
+            <h2 class="titleH2 text-center">¡Bienvenidos!</h2>
+            <p class="text-center central-container description-text">
+            Vivir es una aventura y brindar a esa aventura los recursos necesarios para disfrutarla al máximo es lo que me despierta cada día. Necesitas ¿Subir peso? ¿tienes intolerancias alimentos? ¿tienes alergias alimentos?¿tienes celiaquía? permiteme acompañarte en este proceso, descubriendo la nueva forma de alimentarte y nutrirte.
+            </p>
+        </div>
+        <div class="pic-profile">
+            <img src="./assets/gabyEstrada.jpg" alt="Gabriela Estrada | Nutricionista | Dietista | Especilista en alergias">
+        </div>
+    </div>
+    <div class="img-front img-1" id="show-pic">
+
+    </div>
+    <div class="services central-container" id="services">
+        <div class="four--column">
+        <div class="one text-center">
+            <i class="fas fa-clipboard-list"></i>
+            <h3 class="titleH3"><a href="#">Evaluación</a></h3>
+
+        </div>
+        <div class="two text-center">
+            <i class="fas fa-user-md"></i>
+            <h3 class="titleH3"><a href="#">Estado nutrional</a></h3>
+
+        </div>
+        <div class="three text-center">
+            <i class="fas fa-chart-bar"></i>
+            <h3 class="titleH3"><a href="#">Gráfica de OMS</a></h3>
+
+        </div>
+        <div class="four text-center">
+            <i class="fas fa-user-check"></i>
+            <h3 class="titleH3"><a href="#">Plan alimenticio</a></h3>
+
+        </div>
+        </div>
+    </div>
+    <div class="img-front img-2" id="show-pic">
+
+    </div>
+    <div class="testimonials" id="testimonials">
+    <p>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nihil dolorem deleniti repellat, eveniet eligendi corrupti architecto debitis ab. Praesentium id facilis quasi dicta, ipsa suscipit aliquam voluptate vitae delectus.</span>
+        <span>Ad enim animi vitae dolor voluptatum nemo beatae. Assumenda minus cupiditate doloribus. Aliquam laboriosam consequatur deleniti inventore quae, quidem illum dolorem libero cum distinctio odit voluptates aspernatur explicabo praesentium assumenda!</span>
+        <span>A nobis quam expedita, voluptatibus natus nisi dicta ipsa. Quis, hic ex. Veritatis facere eaque accusamus cum saepe corporis expedita. Quidem, non. In ipsam quas iure? Voluptate esse incidunt perspiciatis.</span>
+        <span>Incidunt sapiente iste voluptatem tempora nisi iure doloribus minus totam sint facere! Libero hic alias aut amet maxime doloribus accusamus ratione. Necessitatibus voluptatem doloribus ea dignissimos quae repellendus blanditiis ratione.</span>
+        <span>Similique fugiat quae nemo, vero incidunt ad culpa reprehenderit soluta. Ex repellendus eligendi accusamus expedita commodi est, beatae repellat sunt dignissimos autem voluptate itaque officia nemo perferendis iusto placeat nisi.</span>
+    </p>
+    </div>
     <?php include('footer.php');?>
     <?php include('links.php'); ?>
 </body>
--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
     <ul class="list-vertical flex-center">
         <li><a class="menu-style" href="#">Home</a></li>
         <li><a class="menu-style" href="#">Servicios</a></li>
-        <li><a class="menu-style" href="#">Recetas y Recursos</a></li>
+        <li><a class="menu-style" href="#">Recetas y recursos</a></li>
         <li><a class="menu-style" href="#">Testimonios</a></li>
         <li><a class="menu-style" href="#">Escribeme</a></li>
     </ul>
